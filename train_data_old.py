3ea93ffc2241ce93677f5ebc5555db0437c838b3
kushagragarwal2443 <kushagragarwal2443@gmail.com>
2021-01-25 01:44:11 +0530

    Jan 25, 2021

--- train_data_old.py
+++ train_data_old.py
@@ -2,10 +2,9 @@ import numpy as np
 import keras
 from keras.models import Sequential
 from keras.models import model_from_json
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, MaxPool2D, AvgPool2D, BatchNormalization
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, MaxPool2D
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-
 # import tensorflow as tf
 # from google.colab import drive
 
@@ -14,8 +13,8 @@ from sklearn.model_selection import train_test_split
 # tf.test.gpu_device_name()
 
 # location = "/content/gdrive/My Drive/Colab Notebooks/MultiDigit_CNN/"
-
 location = "./"
+
 train_data0 = np.load(location + 'Data/data0.npy')
 train_lab0 = np.load(location + 'Label/lab0.npy')
 
@@ -37,12 +36,10 @@ input_shape = (img_rows, img_cols, 1)
 
 X_train = X_train.reshape(-1, img_rows, img_cols, 1)
 X_test = X_test.reshape(-1, img_rows, img_cols, 1)
-
 # X_train = X_train.astype('float32')
 # X_test = X_test.astype('float32')
 # X_train /= 255
 # X_test /= 255
-
 print("Train Test split:", X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
 print("Label counts y_train:", np.unique(y_train, return_counts=True))
@@ -55,76 +52,30 @@ y_train = keras.utils.to_categorical(y_train, num_category)
 y_test = keras.utils.to_categorical(y_test, num_category)
 print("One-hot encoded label size:", y_train.shape, y_test.shape)
 
-######################## SIMPLE CNN WITH FCs ###############################
-# model = Sequential()
-# model.add(Conv2D(8, kernel_size=(3, 3), padding = "same", activation='relu', input_shape=input_shape))
-# model.add(Conv2D(8, (3, 3), activation='relu', padding = "same"))
-# model.add(MaxPool2D(pool_size=(2, 2), strides = 2))
-# model.add(Conv2D(16, kernel_size=(3, 3), padding = "same", activation='relu'))
-# model.add(Conv2D(16, (3, 3), activation='relu', padding = "same"))
-# model.add(MaxPool2D(pool_size=(2, 2), strides = 2))
-# model.add(Conv2D(32, kernel_size=(3, 3), padding = "same", activation='relu'))
-# model.add(Conv2D(64, (3, 3), activation='relu', padding = "same"))
-# model.add(Conv2D(128, (3, 3), activation='relu', padding = "same"))
-# model.add(MaxPool2D(pool_size=(2, 2), strides = 2))
+model = Sequential()
 
-# model.add(Flatten())
-# model.add(Dense(500, activation='relu'))
-# model.add(Dense(100, activation='relu'))
-# model.add(Dense(num_category*2, activation='relu'))
-# model.add(Dense(num_category, activation='softmax'))
-# model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
+model.add(Conv2D(32, kernel_size=(3, 3), padding = "same", activation='relu', input_shape=input_shape))
+model.add(Conv2D(32, (3, 3), activation='relu', padding = "same"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides = 2, padding = "same"))
 
-######################## YANN LECUN's LENET-5 (Modified) ############################
-# model = Sequential()
-# model.add(Conv2D(filters=6, kernel_size=(5,5), padding='valid', input_shape=input_shape, activation='relu'))
-# model.add(MaxPool2D(pool_size=(2,2)))
-# model.add(Conv2D(filters=16, kernel_size=(5,5), padding='valid', activation='relu'))
-# model.add(MaxPool2D(pool_size=(2,2)))
-# model.add(Flatten())
-# model.add(Dense(120, activation='relu'))
-# model.add(Dense(84, activation='relu'))
-# model.add(Dense(num_category, activation='softmax'))
+model.add(Conv2D(64, kernel_size=(3, 3), padding = "same", activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu', padding = "same"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides = 2, padding = "same"))
 
-###################### SIMILAR ARCHITECTURE GOT 99.75% on MNIST #########################
-model= Sequential()
-#-----------------#
-model.add(Conv2D(32,kernel_size=3,activation='relu',input_shape=input_shape))
-model.add(BatchNormalization())
-model.add(Conv2D(32,kernel_size=3,activation='relu'))
-model.add(BatchNormalization())
-# model.add(Conv2D(32,kernel_size=5,strides=2,padding='same',activation='relu'))
-# model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2,2)))
-# model.add(Dropout(0.25))
-#-----------------#
-model.add(Conv2D(64,kernel_size=3,activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(64,kernel_size=3,activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2,2)))
-# model.add(Dropout(0.25))
-#-----------------#
-model.add(Conv2D(128,kernel_size=3,activation='relu'))
-model.add(BatchNormalization())
-model.add(Conv2D(128,kernel_size=3,activation='relu'))
-model.add(BatchNormalization())
-model.add(MaxPool2D(pool_size=(2,2)))
-# model.add(Dropout(0.25))
-#-----------------#
+model.add(Conv2D(128, kernel_size=(3, 3), padding = "same", activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu', padding = "same"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides = 2, padding = "same"))
+
 model.add(Flatten())
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(500, activation='relu'))
 model.add(Dense(100, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(74, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(37, activation='softmax'))
+model.add(Dense(num_category, activation='softmax'))
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer='adam', metrics=['accuracy'])
 
-################# COMPILE THE MODEL ######################
-model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-print(model.summary())
-
-batch_size = 1000
-num_epoch = 5
+batch_size = 100
+num_epoch = 12
 model_log = model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epoch, verbose=1, validation_data=(X_test, y_test))
 
 score = model.evaluate(X_test, y_test, verbose=0)
@@ -149,17 +100,7 @@ plt.xlabel('# Epoch')
 plt.legend(['train', 'test'], loc='upper right')
 
 plt.tight_layout()
-plt.savefig("Plots.png")
-
-file_name = location + 'model_1'
-#Save the model
-# serialize model to JSON
-model_json = model.to_json()
-with open(file_name + ".json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights(file_name +".h5")
-print("Saved model to disk")
+plt.save_fig("Plots.png")
 
 # choice = int(input("Do you want to store this model?? Enter 1 for Yes and 0 for No: "))
 # if(choice == 1):
